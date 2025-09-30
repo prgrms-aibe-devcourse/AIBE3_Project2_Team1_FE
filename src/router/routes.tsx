f7b97c81237819e930f26d3cb6d3ffaf23f1c9eb
SWWWin <bgpd1258@naver.com>
2025-09-30 10:51:40 +0900

    [design] 프로젝트 카드 구현

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,9 @@
 import Home from '../pages/Home';
 import ProposalPage from '../pages/proposal';
+import ProjectList from '../pages/ProjectList';
 
 export const routes = [
   { path: '/', element: <Home /> },
   { path: '/proposal', element: <ProposalPage /> },
+  { path: '/ProjectList', element: <ProjectList /> },
 ];
