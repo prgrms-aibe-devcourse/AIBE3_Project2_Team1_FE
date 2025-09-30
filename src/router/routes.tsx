705f2457731e0561e1f078520ec47c77a3eb1795
Jeongho Kim <kimjungho.kr@gmail.com>
2025-09-30 11:37:18 +0900

    [fix] import 대소문자 변경

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,5 @@
-import ProposalPage from '../pages/Proposal';
-import Home from '../pages/Home';
+import Home from '../pages/home';
+import ProposalPage from '../pages/proposal';
 
 export const routes = [
   { path: '/', element: <Home /> },
