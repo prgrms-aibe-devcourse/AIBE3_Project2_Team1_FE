930c40fe706f7a9f1c962378a9fa3e8fb080da52
SWWWin <bgpd1258@naver.com>
2025-10-01 12:32:36 +0900

    [fix] 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import MainLayout from './layouts/MainLayout';
 import AppRouter from './router/AppRouter';
 
-import { RouterProvider } from 'react-router-dom';
-import router from './router/routers';
-
 const App = () => {
   return (
     <MainLayout>
       <AppRouter />
     </MainLayout>
   );
-  return <RouterProvider router={router} />;
 };
 
 export default App;
