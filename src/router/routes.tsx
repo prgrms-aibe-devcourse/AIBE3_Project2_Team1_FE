695201762dbad7c5c145e2941bb002400b078204
TooTo <wlguq900@gmail.com>
2025-10-01 10:45:26 +0900

    [feat] profile 페이지,대시보드 기반 구현

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,9 @@
 import Home from '../pages/Home';
 import ProposalPage from '../pages/proposal';
+import ProfilePage from '@/features/profile/profilePage';
 
 export const routes = [
   { path: '/', element: <Home /> },
   { path: '/proposal', element: <ProposalPage /> },
+  { path: '/profile', element: <ProfilePage /> },
 ];
