b93216f94570fc6d2d4a98d72d221b06adcc1d03
haemin4738 <leeheamin12@gmail.com>
2025-10-10 17:55:15 +0900

    [Fix] Dev CI pipeline 수정

--- src/features/auth/AuthContext.tsx
+++ src/features/auth/AuthContext.tsx
@@ -10,8 +10,10 @@ export interface AuthContextType {
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const useAuth = (): AuthContextType => {
+export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
   return context;
 };
