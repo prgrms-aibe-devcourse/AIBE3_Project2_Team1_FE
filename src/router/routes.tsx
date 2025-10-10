40fdf765ab5278958d4ab999348051adbb1f231b
seungin <tmddls91@inha.edu>
2025-10-10 12:35:47 +0900

    [feat] 페이지 레이아웃 분리 적용

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,30 +1,31 @@
 import LoginPage from '@/features/auth/LoginPage';
-import ProfilePage from '@/features/profile';
-import ReviewWritePage from '@/features/review/ReviewWritePage';
 import SignUpPage from '@/features/user/SignUpPage';
-import ClientProposalPage from '../pages/ClientProposal';
-import FreelancerProposalPage from '../pages/FreelancerProposal';
-import Home from '../pages/Home';
-import ProjectList from '../pages/ProjectList';
+import AdminLoginPage from '@/features/admin/pages/AdminLoginPage';
+
+import Home from '@/pages/Home';
+import ClientProposalPage from '@/pages/ClientProposal';
+import FreelancerProposalPage from '@/pages/FreelancerProposal';
+import ReviewWritePage from '@/features/review/ReviewWritePage';
+import ProjectList from '@/pages/ProjectList';
+import ProfilePage from '@/features/profile';
 import OverviewPage from '@/pages/OverviewPage';
 import FreelancerNotifications from '@/features/message/FreelancerNotificationsPage';
-import AdminLoginPage from '@/features/admin/pages/AdminLoginPage';
 import AdminDashboardPage from '@/features/admin/pages/AdminDashboardPage';
 
-export const routes = [
+export const noLayoutRoutes = [
+  { path: '/login', element: <LoginPage /> },
+  { path: '/signup', element: <SignUpPage /> },
+  { path: '/admin/login', element: <AdminLoginPage /> },
+];
+
+export const mainRoutes = [
   { path: '/', element: <Home /> },
   { path: '/client-proposal', element: <ClientProposalPage /> },
   { path: '/freelancer-proposal', element: <FreelancerProposalPage /> },
   { path: '/review/write', element: <ReviewWritePage /> },
   { path: '/projects/:groupId', element: <ProjectList /> },
-  { path: '/login', element: <LoginPage /> },
-  { path: '/signup', element: <SignUpPage /> },
   { path: '/profile', element: <ProfilePage /> },
   { path: '/overview', element: <OverviewPage /> },
-  {
-    path: '/messages/notifications',
-    element: <FreelancerNotifications />,
-  },
-  { path: '/admin/login', element: <AdminLoginPage /> },
+  { path: '/messages/notifications', element: <FreelancerNotifications /> },
   { path: '/admin/dashboard', element: <AdminDashboardPage /> },
 ];
