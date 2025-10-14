fa6fe7544fc13c0053075588c60105ff2d3c120c
Unoguna <kimjungho.kr@gmail.com>
2025-10-14 14:39:08 +0900

    [feat] 프로필 페이지 중복되는 헤더 삭제

--- src/features/profile/pages/index.tsx
+++ src/features/profile/pages/index.tsx
@@ -17,19 +17,6 @@ export default function ProfilePage() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      {/* 헤더 */}
-      <header className="flex items-center justify-between px-8 py-4 shadow-sm bg-white relative">
-        <div className="w-20" />
-        <div className="absolute left-1/2 transform -translate-x-1/2">
-          <div className="text-rose-500 font-bold text-4xl">Pickple</div>
-        </div>
-        <div className="flex gap-2 text-gray-600 text-sm">
-          <button className="hover:text-gray-800">로그인</button>
-          <span>|</span>
-          <button className="hover:text-gray-800">회원가입</button>
-        </div>
-      </header>
-
       {/* 프로필 카드 */}
       <ProfileCard
         mode={mode}
