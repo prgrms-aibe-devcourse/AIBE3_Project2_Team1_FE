7a8536acd3c4ad3262701110cb35d4ae038acaf4
SWWWin <bgpd1258@naver.com>
2025-10-16 18:21:54 +0900

    [design] 헤더 버튼 색상 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,10 +30,10 @@ const Header = () => {
           <Link to="/project/write" className="hover:text-rose-400">
             프로젝트 생성하기
           </Link>
-          <Link to="/projects/freelancer" className="hover:text-rose-400">
+          <Link to="/projects/freelancer" className="hover:text-green-500">
             프리랜서 찾기
           </Link>
-          <Link to="/projects/client" className="hover:text-rose-400">
+          <Link to="/projects/client" className="hover:text-blue-500">
             프로젝트 찾기
           </Link>
 
